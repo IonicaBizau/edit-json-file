9b9c32957ceaa495253a4d3d5dc3e0b52a4c7bac
Ionică Bizău <bizauionica@gmail.com>
2016-10-18 12:43:40 +0300

    Working prototype

--- example/index.js
+++ example/index.js
@@ -2,4 +2,41 @@
 
 const editJsonFile = require("../lib");
 
-console.log(editJsonFile());
+// If the file doesn't exist, the content will be an empty object by default.
+let file = editJsonFile(`${__dirname}/foo.json`);
+
+// Set a couple of fields
+file.set("planet", "Earth");
+file.set("name.first", "Johnny");
+file.set("name.last", "B.");
+file.set("is_student", false);
+
+// Output the content
+console.log(file.get());
+// { planet: 'Earth',
+//   name: { first: 'Johnny', last: 'B.' },
+//   is_student: false } }
+
+// Save the data to the disk
+file.save();
+
+// Reload it from the disk
+file = editJsonFile(`${__dirname}/foo.json`, {
+    autosave: true
+});
+
+// Get one field
+console.log(file.get("name.first"));
+// => Johnny
+
+// This will save it to disk
+file.set("a.new.field.as.object", {
+    hello: "world"
+});
+
+// Output the whole thing
+console.log(file.toObject());
+// { planet: 'Earth',
+//   name: { first: 'Johnny', last: 'B.' },
+//   is_student: false,
+//   a: { new: { field: [Object] } } }
